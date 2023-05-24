72eba1c3ddff860f82c54392bbae4735e7bcbea6
FLAME <xaustin03@gmail.com>
2023-05-23 23:00:17 -0400

    Update index.js

--- ProgrammingWithMosh-js-basics/index.js
+++ ProgrammingWithMosh-js-basics/index.js
@@ -11,3 +11,22 @@ function square(number) {
 
 greet('John', 'Smith');
 console.log(square(2));
+
+function formatDecimal(id) {
+    let x = document.getElementById(id).innerHTML;
+    let y = x.includes('.') ? x.replace(/\.?0+$/, '') : x;
+    document.getElementById(id).innerHTML = y;
+}
+
+formatDecimal('p123');
+
+
+// ✅ remove trailing zeros from a number
+const num1 = 13.4530000;
+const result1 = parseFloat(num1);
+console.log(result1); // 👉️ 13.453
+
+// ✅ parse a string to a number and remove trailing zeros
+const str1 = '13.453000';
+const result2 = parseFloat(str1);
+console.log(result2); // 👉️ 13.453
